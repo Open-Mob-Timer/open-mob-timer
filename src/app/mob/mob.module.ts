b64f146ffcc12755c0211635ed18c84fe3884968
Daniel Breen <daniel.breen@kno2.com>
2020-11-17 22:09:19 -0700

    Major Refactor

--- src/app/mob/mob.module.ts
+++ src/app/mob/mob.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MobComponent } from './mob.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '@dtm/shared/shared.module';
+import { UsersComponent } from './users/users.component';
+import { InstrumentPanelComponent } from './instrument-panel/instrument-panel.component';
 
 @NgModule({
-  declarations: [MobComponent],
+  declarations: [MobComponent, UsersComponent, InstrumentPanelComponent],
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule,
   ]
 })
 export class MobModule { }
